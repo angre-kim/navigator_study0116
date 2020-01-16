142565c10515fa2bdcaf64b7d2a60dd60ea66ab7
angre-kim <angre@daum.net>
2020-01-16 11:40:31 +0900

    Navigator.pop

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen0(),
+      //home: Screen0(),
+      initialRoute: '/',
       routes: {                          //Map{String
         '/': (context) => Screen0(),     // '/'는 키 네임:(context)는 String, return 값으로 Widget
         '/first': (context)=>Screen1(),
