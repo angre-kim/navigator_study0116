50994b65428a7ad67b6f399cc1936bd394cbae14
angre-kim <angre@daum.net>
2020-01-16 11:39:56 +0900

    Navigator.pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'screen1.dart';
+import 'screen0.dart';
+import 'screen2.dart';
+import 'screen2.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen1(),
+      home: Screen0(),
+      routes: {                          //Map{String
+        '/': (context) => Screen0(),     // '/'는 키 네임:(context)는 String, return 값으로 Widget
+        '/first': (context)=>Screen1(),
+        '/second': (context) =>Screen2(),
+
+      },
     );
   }
 }
